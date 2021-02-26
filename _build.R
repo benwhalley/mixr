e5fadccf8dae025fda53f2f20e578ba90a2ec6b9
Ben Whalley <ben.whalley@plymouth.ac.uk>
2021-02-26 23:45:30 +0000

    updates for 2020

--- _build.R
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env Rscript
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
-# bookdown::render_book("index.Rmd", rmarkdown::pdf_document(toc=T, toc_depth=3, latex_engine="xelatex"))
